7f20193290c66f14716809c837f5818b1bcaa021
Hayatsa <hayatabudiab@gmail.com>
2021-12-14 19:35:15 -0600

    final commit

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,10 +1,41 @@
-import { getOrders } from "./database.js"
+import { getOrders, getMetals, getSizes, getStyles } from "./database.js"
+
+const metals = getMetals()
+const sizes = getSizes()
+const styles = getStyles()
 
 const buildOrderListItem = (order) => {
-    return `<li>
-        Order #${order.id} was placed on ${order.timestamp}
+
+    const foundMetal = metals.find(
+    (metal) => {
+        return metal.id === order.metalId
+        }
+    )
+
+    const foundSize = sizes.find(
+        (size) => {
+            return size.id === order.sizeId
+        }
+    )
+
+    const foundStyle = styles.find(
+        (style) => {
+            return style.id === order.styleId
+        }
+    )
+
+    const totalCost = foundMetal.price + foundSize.price + foundStyle.price
+
+    const costString = totalCost.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD"
+    })
+
+        return `<li>
+        Order #${order.id} cost ${costString} was placed on ${order.timestamp}
     </li>`
 }
+
 
 export const Orders = () => {
     /*
